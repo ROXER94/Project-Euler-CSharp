882e39c7b1a218f4011166de306e6659dbe5a87f
Robert Oxer <rdo26@cornell.edu>
2018-04-28 16:50:40 -0400

    Add files via upload

--- ProjectEuler/Problem011.cs
+++ ProjectEuler/Problem011.cs
@@ -38,7 +38,7 @@ namespace ProjectEuler
                 for (int j = 0; j <= grid[i].Length - 4; j++)
                     ans = Math.Max(ans, grid[i][j] * grid[i][j + 1] * grid[i][j + 2] * grid[i][j + 3]);
             for (int i = 0; i <= grid.Length - 4; i++)
-                for (int j = 0; j < grid[i].Length; j++)
+                for (int j = 0; j <= grid[i].Length - 1; j++)
                     ans = Math.Max(ans, grid[i][j] * grid[i + 1][j] * grid[i + 2][j] * grid[i + 3][j]);
             for (int i = 0; i <= grid.Length - 4; i++)
                 for (int j = 0; j <= grid[i].Length - 4; j++)
