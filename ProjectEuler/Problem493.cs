1843e0f49c8f00df4ae6af718005a567acec7ec6
Robert Oxer <rdo26@cornell.edu>
2018-09-16 13:49:17 -0400

    Add files via upload

---
+++ ProjectEuler/Problem493.cs
@@ -0,0 +1,16 @@
+﻿using ProjectEuler.Common;
+using System;
+
+namespace ProjectEuler
+{
+    partial class ProjectEuler
+    {
+        /// <summary>
+        /// Calculates the expected number of distinct colors in 20 randomly picked balls
+        /// </summary>
+        static void P493()
+        {
+            Console.WriteLine(Math.Round(7 * (1 - (double)Functions.getnCk(60, 20) / (double)Functions.getnCk(70, 20)), 10));
+        }
+    }
+}
