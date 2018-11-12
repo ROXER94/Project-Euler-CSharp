5e23de86b56cda6a8a07eddf0c76475600bddf33
Robert Oxer <rdo26@cornell.edu>
2018-11-11 19:41:49 -0500

    Add files via upload

--- ProjectEuler/Problem191.cs
+++ ProjectEuler/Problem191.cs
@@ -15,17 +15,17 @@ namespace ProjectEuler
         /// </summary>
         /// <param name="s">String</param>
         /// <param name="late">Bool</param>
-        /// <param name="absenses">Int</param>
+        /// <param name="absences">Int</param>
         /// <param name="day">Int</param>
         /// <returns></returns>
-        static int getPrizeStringCount(bool late, int absenses, int day)
+        static int getPrizeStringCount(bool late, int absences, int day)
         {
             if (day == 30) return 1;
             if (late)
             {
-                if (absenses == 2)
+                if (absences == 2)
                 {
-                    Tuple<bool, int, int> T1 = Tuple.Create(late, absenses, day);
+                    Tuple<bool, int, int> T1 = Tuple.Create(late, absences, day);
                     if (!prizeStringDict.ContainsKey(T1))
                         prizeStringDict[T1] = getPrizeStringCount(late, 0, day + 1);
                     return prizeStringDict[T1];
@@ -35,15 +35,15 @@ namespace ProjectEuler
                     Tuple<bool, int, int> T1 = Tuple.Create(late, 0, day + 1);
                     if (!prizeStringDict.ContainsKey(T1))
                         prizeStringDict[T1] = getPrizeStringCount(late, 0, day + 1);
-                    Tuple<bool, int, int> T2 = Tuple.Create(late, absenses + 1, day + 1);
+                    Tuple<bool, int, int> T2 = Tuple.Create(late, absences + 1, day + 1);
                     if (!prizeStringDict.ContainsKey(T2))
-                        prizeStringDict[T2] = getPrizeStringCount(late, absenses + 1, day + 1);
+                        prizeStringDict[T2] = getPrizeStringCount(late, absences + 1, day + 1);
                     return prizeStringDict[T1] + prizeStringDict[T2];
                 }
             }
             else
             {
-                if (absenses == 2)
+                if (absences == 2)
                 {
                     Tuple<bool, int, int> T1 = Tuple.Create(late, 0, day + 1);
                     if (!prizeStringDict.ContainsKey(T1))
@@ -58,9 +58,9 @@ namespace ProjectEuler
                     Tuple<bool, int, int> T1 = Tuple.Create(late, 0, day + 1);
                     if (!prizeStringDict.ContainsKey(T1))
                         prizeStringDict[T1] = getPrizeStringCount(late, 0, day + 1);
-                    Tuple<bool, int, int> T2 = Tuple.Create(late, absenses + 1, day + 1);
+                    Tuple<bool, int, int> T2 = Tuple.Create(late, absences + 1, day + 1);
                     if (!prizeStringDict.ContainsKey(T2))
-                        prizeStringDict[T2] = getPrizeStringCount(late, absenses + 1, day + 1);
+                        prizeStringDict[T2] = getPrizeStringCount(late, absences + 1, day + 1);
                     Tuple<bool, int, int> T3 = Tuple.Create(!late, 0, day + 1);
                     if (!prizeStringDict.ContainsKey(T3))
                         prizeStringDict[T3] = getPrizeStringCount(!late, 0, day + 1);
