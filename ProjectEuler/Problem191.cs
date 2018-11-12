ea50db919dc11b458961a1fa979279be3d2618c9
Robert Oxer <rdo26@cornell.edu>
2018-11-11 19:24:55 -0500

    Add files via upload

--- ProjectEuler/Problem191.cs
+++ ProjectEuler/Problem191.cs
@@ -19,7 +19,7 @@ namespace ProjectEuler
         /// <param name="absenses">Int</param>
         /// <param name="day">Int</param>
         /// <returns></returns>
-        static int getPrizeStringCount(StringBuilder s, bool late, int absenses, int day)
+        static int getPrizeStringCount(bool late, int absenses, int day)
         {
             if (day == 30) return 1;
             if (late)
@@ -28,17 +28,17 @@ namespace ProjectEuler
                 {
                     Tuple<bool, int, int> T1 = Tuple.Create(late, absenses, day);
                     if (!prizeStringDict.ContainsKey(T1))
-                        prizeStringDict[T1] = getPrizeStringCount(s.Append("O"), late, 0, day + 1);
+                        prizeStringDict[T1] = getPrizeStringCount(late, 0, day + 1);
                     return prizeStringDict[T1];
                 }
                 else
                 {
                     Tuple<bool, int, int> T1 = Tuple.Create(late, 0, day + 1);
                     if (!prizeStringDict.ContainsKey(T1))
-                        prizeStringDict[T1] = getPrizeStringCount(s.Append("O"), late, 0, day + 1);
+                        prizeStringDict[T1] = getPrizeStringCount(late, 0, day + 1);
                     Tuple<bool, int, int> T2 = Tuple.Create(late, absenses + 1, day + 1);
                     if (!prizeStringDict.ContainsKey(T2))
-                        prizeStringDict[T2] = getPrizeStringCount(s.Append("A"), late, absenses + 1, day + 1);
+                        prizeStringDict[T2] = getPrizeStringCount(late, absenses + 1, day + 1);
                     return prizeStringDict[T1] + prizeStringDict[T2];
                 }
             }
@@ -48,23 +48,23 @@ namespace ProjectEuler
                 {
                     Tuple<bool, int, int> T1 = Tuple.Create(late, 0, day + 1);
                     if (!prizeStringDict.ContainsKey(T1))
-                        prizeStringDict[T1] = getPrizeStringCount(s.Append("O"), late, 0, day + 1);
+                        prizeStringDict[T1] = getPrizeStringCount(late, 0, day + 1);
                     Tuple<bool, int, int> T2 = Tuple.Create(!late, 0, day + 1);
                     if (!prizeStringDict.ContainsKey(T2))
-                        prizeStringDict[T2] = getPrizeStringCount(s.Append("A"), !late, 0, day + 1);
+                        prizeStringDict[T2] = getPrizeStringCount(!late, 0, day + 1);
                     return prizeStringDict[T1] + prizeStringDict[T2];
                 }
                 else
                 {
                     Tuple<bool, int, int> T1 = Tuple.Create(late, 0, day + 1);
                     if (!prizeStringDict.ContainsKey(T1))
-                        prizeStringDict[T1] = getPrizeStringCount(s.Append("O"), late, 0, day + 1);
+                        prizeStringDict[T1] = getPrizeStringCount(late, 0, day + 1);
                     Tuple<bool, int, int> T2 = Tuple.Create(late, absenses + 1, day + 1);
                     if (!prizeStringDict.ContainsKey(T2))
-                        prizeStringDict[T2] = getPrizeStringCount(s.Append("O"), late, absenses + 1, day + 1);
+                        prizeStringDict[T2] = getPrizeStringCount(late, absenses + 1, day + 1);
                     Tuple<bool, int, int> T3 = Tuple.Create(!late, 0, day + 1);
                     if (!prizeStringDict.ContainsKey(T3))
-                        prizeStringDict[T3] = getPrizeStringCount(s.Append("O"), !late, 0, day + 1);
+                        prizeStringDict[T3] = getPrizeStringCount(!late, 0, day + 1);
                     return prizeStringDict[T1] + prizeStringDict[T2] + prizeStringDict[T3];
                 }
             }
@@ -75,7 +75,7 @@ namespace ProjectEuler
         /// </summary>
         static void P191()
         {
-            Console.WriteLine(getPrizeStringCount(new StringBuilder(), false, 0, 0));
+            Console.WriteLine(getPrizeStringCount(false, 0, 0));
         }
     }
 }
