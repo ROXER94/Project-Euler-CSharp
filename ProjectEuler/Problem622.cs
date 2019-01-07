f69bff8661105422ad6ef293118f9481f2d1148a
Robert Oxer <rdo26@cornell.edu>
2019-01-06 19:07:02 -0500

    Add files via upload

---
+++ ProjectEuler/Problem622.cs
@@ -0,0 +1,17 @@
+﻿using ProjectEuler.Common;
+using System;
+using System.Linq;
+
+namespace ProjectEuler
+{
+    partial class ProjectEuler
+    {
+        /// <summary>
+        /// Calculates the sum of all values of n that satisfy s(n)=60
+        /// </summary>
+        static void P622()
+        {
+            Console.WriteLine((from n in Functions.getFactors((long)Math.Pow(2, 60) - 1) where Functions.getMultiplicativeOrder(2, (int)n) == 60 select n + 1).Sum());
+        }
+    }
+}
