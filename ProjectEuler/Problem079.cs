82a145befa02f642b2f5e6f49b75d59215b2fbdc
Robert Oxer <rdo26@cornell.edu>
2019-06-19 18:52:38 -0400

    Add files via upload

--- ProjectEuler/Problem079.cs
+++ ProjectEuler/Problem079.cs
@@ -16,7 +16,7 @@ namespace ProjectEuler
             var passcodes = Functions.getPermutations(new List<int>() { 0, 1, 2, 3, 6, 7, 8, 9 }, 8).OrderBy(n => n.ToString());
             var keylog = new HashSet<string>();
             foreach (string s in File.ReadAllText(@"...\...\Resources\p079_keylog.txt").Split('\n'))
-                keylog.Add(s.Remove(3));
+                keylog.Add(s);
             foreach (var i in passcodes)
                 foreach (var j in keylog)
                     if (i.ToList().IndexOf((int)Char.GetNumericValue(j[0])) < i.ToList().IndexOf((int)Char.GetNumericValue(j[1])) &&
