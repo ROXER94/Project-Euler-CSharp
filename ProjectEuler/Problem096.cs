962a08fa9e22e7aa58d5e98c18d1d4eb263d639d
Robert Oxer <rdo26@cornell.edu>
2019-06-23 16:08:56 -0400

    Add files via upload

--- ProjectEuler/Problem096.cs
+++ ProjectEuler/Problem096.cs
@@ -1,4 +1,4 @@
-﻿using ProjectEuler.Common;
+using ProjectEuler.Common;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -12,20 +12,20 @@ namespace ProjectEuler
         /// Gets the solution to a Sudoku board
         /// </summary>
         /// <param name="board">Int[,]</param>
-        /// <param name="availableBoard">List<int>[,]</param>
+        /// <param name="candidateBoard">List<int>[,]</param>
         /// <returns>The solution to a Sudoku board</returns>
-        static int[,] getSudokuSolution(int[,] board, List<int>[,] availableBoard)
+        static int[,] getSudokuSolution(int[,] board, List<int>[,] candidateBoard)
         {
             if (isSudokuComplete(board)) return board;
-            Tuple<int, int> firstAvailable = getSudokuFirstAvailable(board);
-            int r = firstAvailable.Item1;
-            int c = firstAvailable.Item2;
-            foreach (int i in availableBoard[r, c])
+            Tuple<int, int> firstAvailableCell = getSudokuFirstAvailableCell(board);
+            int r = firstAvailableCell.Item1;
+            int c = firstAvailableCell.Item2;
+            foreach (int i in candidateBoard[r, c])
             {
                 board[r, c] = i;
                 if (isSudokuValid(board))
                 {
-                    int[,] currentBoard = getSudokuSolution(board, availableBoard);
+                    int[,] currentBoard = getSudokuSolution(board, candidateBoard);
                     if (isSudokuComplete(board)) return currentBoard;
                 }
                 board[r, c] = 0;
@@ -52,7 +52,7 @@ namespace ProjectEuler
         /// </summary>
         /// <param name="board">Int[,]</param>
         /// <returns>The first available cell in a Sudoku board</returns>
-        static Tuple<int, int> getSudokuFirstAvailable(int[,] board)
+        static Tuple<int, int> getSudokuFirstAvailableCell(int[,] board)
         {
             for (int r = 0; r < board.GetLength(0); r++)
                 for (int c = 0; c < board.GetLength(1); c++)
@@ -68,7 +68,7 @@ namespace ProjectEuler
         /// <returns>True if a Sudoku board is valid</returns>
         static bool isSudokuValid(int[,] board)
         {
-            return isSudokuRowsValid(board) && isSudokuColsValid(board) && isSudokuBlocksValid(board);
+            return isSudokuRowsValid(board) && isSudokuColumnsValid(board) && isSudokuBlocksValid(board);
         }
 
         /// <summary>
@@ -89,7 +89,7 @@ namespace ProjectEuler
         /// </summary>
         /// <param name="board">Int[,]</param>
         /// <returns>True if a Sudoku board contains valid columns</returns>
-        static bool isSudokuColsValid(int[,] board)
+        static bool isSudokuColumnsValid(int[,] board)
         {
             for (int c = 0; c < board.GetLength(1); c++)
                 if (Functions.hasDuplicates(Functions.GetArrayColumn(board, c), new int[] { 0 }))
@@ -118,65 +118,65 @@ namespace ProjectEuler
         }
 
         /// <summary>
-        /// Updates a Sudoku board and an available Sudoku board
+        /// Updates a Sudoku board and a candidate Sudoku board
         /// </summary>
         /// <param name="board">Int[,]</param>
-        /// <param name="availableBoard">List<int>[,]</param>
-        static void updateSudokuBoards(int[,] board, List<int>[,] availableBoard)
+        /// <param name="candidateBoard">List<int>[,]</param>
+        static void updateSudokuBoards(int[,] board, List<int>[,] candidateBoard)
         {
             for (int r = 0; r < board.GetLength(0); r++)
                 for (int c = 0; c < board.GetLength(1); c++)
                     if (board[r, c] != 0)
                     {
-                        removeSudokuAvailableRow(board[r, c], r, availableBoard);
-                        removeSudokuAvailableCol(board[r, c], c, availableBoard);
-                        removeSudokuAvailableBlock(board[r, c], r, c, availableBoard);
-                        availableBoard[r, c] = new List<int>();
+                        removeSudokuCandidateRow(board[r, c], r, candidateBoard);
+                        removeSudokuCandidateColumn(board[r, c], c, candidateBoard);
+                        removeSudokuCandidateBlock(board[r, c], r, c, candidateBoard);
+                        candidateBoard[r, c] = new List<int>();
                     }
-            for (int r = 0; r < availableBoard.GetLength(0); r++)
-                for (int c = 0; c < availableBoard.GetLength(1); c++)
-                    if (availableBoard[r, c].Count == 1)
+            for (int r = 0; r < candidateBoard.GetLength(0); r++)
+                for (int c = 0; c < candidateBoard.GetLength(1); c++)
+                    if (candidateBoard[r, c].Count == 1)
                     {
-                        board[r, c] = availableBoard[r, c][0];
-                        availableBoard[r, c] = new List<int>();
-                        updateSudokuBoards(board, availableBoard);
+                        board[r, c] = candidateBoard[r, c][0];
+                        candidateBoard[r, c] = new List<int>();
+                        updateSudokuBoards(board, candidateBoard);
                     }
         }
 
         /// <summary>
-        /// Removes numbers from a row of an available Sudoku board
+        /// Removes numbers from a row of a candidate Sudoku board
         /// </summary>
         /// <param name="number">Int</param>
         /// <param name="rowIndex">Int</param>
-        /// <param name="availableBoard">List<int>[,]</param>
-        static void removeSudokuAvailableRow(int number, int rowIndex, List<int>[,] availableBoard)
+        /// <param name="candidateBoard">List<int>[,]</param>
+        static void removeSudokuCandidateRow(int number, int rowIndex, List<int>[,] candidateBoard)
         {
             for (int i = 0; i < 9; i++)
-                if (availableBoard[rowIndex, i].Contains(number))
-                    availableBoard[rowIndex, i].Remove(number);
+                if (candidateBoard[rowIndex, i].Contains(number))
+                    candidateBoard[rowIndex, i].Remove(number);
         }
 
         /// <summary>
-        /// Removes numbers from a column of an available Sudoku board
+        /// Removes numbers from a column of a candidate Sudoku board
         /// </summary>
         /// <param name="number">Int</param>
         /// <param name="colIndex">Int</param>
-        /// <param name="availableBoard"></param>
-        static void removeSudokuAvailableCol(int number, int colIndex, List<int>[,] availableBoard)
+        /// <param name="candidateBoard">List<int>[,]</param>
+        static void removeSudokuCandidateColumn(int number, int columnIndex, List<int>[,] candidateBoard)
         {
             for (int i = 0; i < 9; i++)
-                if (availableBoard[i, colIndex].Contains(number))
-                    availableBoard[i, colIndex].Remove(number);
+                if (candidateBoard[i, columnIndex].Contains(number))
+                    candidateBoard[i, columnIndex].Remove(number);
         }
 
         /// <summary>
-        /// Removes numbers from a block of an available Sudoku board
+        /// Removes numbers from a block of a candidate Sudoku board
         /// </summary>
         /// <param name="number">Int</param>
         /// <param name="rowIndex">Int</param>
         /// <param name="colIndex">Int</param>
-        /// <param name="availableBoard">List<int>[,]</param>
-        static void removeSudokuAvailableBlock(int number, int rowIndex, int colIndex, List<int>[,] availableBoard)
+        /// <param name="candidateBoard">List<int>[,]</param>
+        static void removeSudokuCandidateBlock(int number, int rowIndex, int columnIndex, List<int>[,] candidateBoard)
         {
             int r = 0;
             int c = 0;
@@ -185,19 +185,19 @@ namespace ProjectEuler
                 r += 3;
                 rowIndex -= 3;
             }
-            while (colIndex - 3 >= 0)
+            while (columnIndex - 3 >= 0)
             {
                 c += 3;
-                colIndex -= 3;
+                columnIndex -= 3;
             }
             for (int a = 0; a < 3; a++)
                 for (int b = 0; b < 3; b++)
-                    if (availableBoard[r + a, c + b].Contains(number))
-                        availableBoard[r + a, c + b].Remove(number);
+                    if (candidateBoard[r + a, c + b].Contains(number))
+                        candidateBoard[r + a, c + b].Remove(number);
         }
 
         /// <summary>
-        /// Gets the Sudoku board from a text file
+        /// Gets a Sudoku board from a text file
         /// </summary>
         /// <param name="sudoku">Int[,]</param>
         /// <param name="sudokuFile">StreamReader</param>
@@ -212,7 +212,7 @@ namespace ProjectEuler
         }
 
         /// <summary>
-        /// Prints the Sudoku board
+        /// Prints a Sudoku board
         /// </summary>
         /// <param name="sudoku">Int[,]</param>
         static void PrintSudoku(int[,] sudoku)
@@ -229,19 +229,19 @@ namespace ProjectEuler
         {
             int ans = 0;
             List<int[,]> sudokus = new List<int[,]>();
-            List<int>[,] availableBoardBase = new List<int>[9, 9];
-            for (int r = 0; r < availableBoardBase.GetLength(0); r++)
-                for (int c = 0; c < availableBoardBase.GetLength(1); c++)
-                    availableBoardBase[r, c] = Enumerable.Range(1, 9).ToList();
+            List<int>[,] candidateBoardBase = new List<int>[9, 9];
+            for (int r = 0; r < candidateBoardBase.GetLength(0); r++)
+                for (int c = 0; c < candidateBoardBase.GetLength(1); c++)
+                    candidateBoardBase[r, c] = Enumerable.Range(1, 9).ToList();
             using (var sudokuFile = File.OpenText(@"...\...\Resources\p096_sudoku.txt"))
             {
                 int[,] sudokuBoard = new int[9, 9];
                 while (sudokuFile.ReadLine() != null)
                 {
                     ReadSudoku(sudokuBoard, sudokuFile);
-                    List<int>[,] availableBoard = Functions.getDeepCopy<List<int>[,]>(availableBoardBase);
-                    updateSudokuBoards(sudokuBoard, availableBoard);
-                    int[,] solution = getSudokuSolution(sudokuBoard, availableBoard);
+                    List<int>[,] candidateBoard = Functions.getDeepCopy<List<int>[,]>(candidateBoardBase);
+                    updateSudokuBoards(sudokuBoard, candidateBoard);
+                    int[,] solution = getSudokuSolution(sudokuBoard, candidateBoard);
                     //PrintSudoku(solution);
                     ans += solution[0, 0] * 100 + solution[0, 1] * 10 + solution[0, 2];
                 }
