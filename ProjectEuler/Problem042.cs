f0e9734cc05406d8a5be99a04dec640108bff87b
Robert Oxer <rdo26@cornell.edu>
2019-07-07 17:25:23 -0400

    Add files via upload

--- ProjectEuler/Problem042.cs
+++ ProjectEuler/Problem042.cs
@@ -15,9 +15,9 @@ namespace ProjectEuler
         {
             int ans = 0;
             IDictionary<char, int> alphabetValues = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToDictionary(l => l, l => l - 'A' + 1);
-            var TriangleNumbers = from i in Enumerable.Range(1, 30) select Functions.getTriangle(i);
+            var triangle = from i in Enumerable.Range(1, 30) select Functions.getTriangle(i);
             foreach (string word in File.ReadAllText(@"...\...\Resources\p042_words.txt").Split(',').Select(s => s.Replace("\"", "")).ToArray())
-                if (TriangleNumbers.Contains((from c in word select Convert.ToInt32(alphabetValues[c])).Sum())) ans++;
+                if (triangle.Contains((from c in word select Convert.ToInt32(alphabetValues[c])).Sum())) ans++;
             Console.WriteLine(ans);
         }
     }
