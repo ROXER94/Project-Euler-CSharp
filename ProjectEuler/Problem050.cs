20474667f32440da5636cb877dbc1e995a7f1682
Robert Oxer <rdo26@cornell.edu>
2019-07-06 21:18:41 -0400

    Add files via upload

--- ProjectEuler/Problem050.cs
+++ ProjectEuler/Problem050.cs
@@ -12,27 +12,21 @@ namespace ProjectEuler
         /// </summary>
         static void P050()
         {
-            int ans = 0;
-            List<int> primes = new List<int> { 2 };
-            int n = 3;
-            while (primes.Sum() < 1000000)
-            {
-                if (Functions.isPrime(n))
-                    primes.Add(n);
-                n += 2;
-            }
-            primes.RemoveAt(primes.Count - 1);
+            long ans = 0;
+            List<long> primes = Functions.getPrimesList(1000000);
+            long primesSum = 0;
+            primes = primes.TakeWhile(p => (primesSum += p) <= 1000000).ToList();
             int index = 0;
             int maximumCount = 0;
             while (index != primes.Count)
             {
-                int sum = 0;
-                List<int> accumulated = (primes.GetRange(index, primes.Count - index)).Select(w => sum += w).ToList();
+                long currentSum = 0;
+                List<long> accumulated = (primes.GetRange(index, primes.Count - index)).Select(p => currentSum += p).ToList();
                 var newPrimes = from j in
                                     (from i in accumulated
                                      where i % 2 != 0 && i % 5 != 0
                                      select i)
-                                where Functions.isPrime(j)
+                                where Functions.isPrime((int)j)
                                 select j;
                 int currentCount = accumulated.IndexOf(newPrimes.Last()) + 1;
                 if (currentCount > maximumCount)
