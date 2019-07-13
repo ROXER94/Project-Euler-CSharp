bb153b35d4267fdec9a01bf2d70f7c350c006c9f
Robert Oxer <rdo26@cornell.edu>
2019-07-12 21:07:22 -0400

    Add files via upload

--- ProjectEuler/Problem044.cs
+++ ProjectEuler/Problem044.cs
@@ -13,12 +13,12 @@ namespace ProjectEuler
         static void P044()
         {
             HashSet<long> pentagon = Functions.ToHashSet(from i in Enumerable.Range(1, 2500) select Functions.getPentagon(i));
-            List<Tuple<long, long>> myTupleList = new List<Tuple<long, long>>();
+            List<Tuple<long, long>> pairs = new List<Tuple<long, long>>();
             foreach (var p1 in pentagon)
                 foreach (var p2 in pentagon.ToList().GetRange(pentagon.ToList().IndexOf(p1) + 1, pentagon.ToList().Count - pentagon.ToList().IndexOf(p1) - 1))
                     if (pentagon.Contains(p1 + p2))
-                            myTupleList.Add(Tuple.Create(p1, p2));
-            foreach (var i in myTupleList)
+                        pairs.Add(Tuple.Create(p1, p2));
+            foreach (var i in pairs)
                 if (pentagon.Contains(i.Item2 - i.Item1))
                 {
                     Console.WriteLine(i.Item2 - i.Item1);
