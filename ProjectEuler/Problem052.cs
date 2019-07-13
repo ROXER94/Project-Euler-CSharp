f62c0c5704f22b2e2ef9b9f76741e2e032d18c0b
Robert Oxer <rdo26@cornell.edu>
2019-07-12 20:28:40 -0400

    Add files via upload

--- ProjectEuler/Problem052.cs
+++ ProjectEuler/Problem052.cs
@@ -12,16 +12,14 @@ namespace ProjectEuler
         /// <returns>True if n has permuted multiples</returns>
         static bool isPermutedMultiple(int n)
         {
-            if ((2 * n).ToString().Length == (3 * n).ToString().Length &&
+            return (2 * n).ToString().Length == (3 * n).ToString().Length &&
                 (3 * n).ToString().Length == (4 * n).ToString().Length &&
                 (4 * n).ToString().Length == (5 * n).ToString().Length &&
                 (5 * n).ToString().Length == (6 * n).ToString().Length &&
                 Functions.isAnagram((2 * n).ToString(), (3 * n).ToString()) &&
                 Functions.isAnagram((3 * n).ToString(), (4 * n).ToString()) &&
                 Functions.isAnagram((4 * n).ToString(), (5 * n).ToString()) &&
-                Functions.isAnagram((5 * n).ToString(), (6 * n).ToString()))
-                    return true;
-            return false;
+                Functions.isAnagram((5 * n).ToString(), (6 * n).ToString());
         }
 
         /// <summary>
