535ba916c6f25aacfb0ef49c9f943abdf06cfd30
Robert Oxer <rdo26@cornell.edu>
2019-07-21 21:12:57 -0400

    Add files via upload

--- ProjectEuler/Problem113.cs
+++ ProjectEuler/Problem113.cs
@@ -11,7 +11,7 @@ namespace ProjectEuler
         /// Gets the number of bouncy numbers below 10 raised to a number
         /// </summary>
         /// <param name="n">Int</param>
-        /// <returns>The number of bouncy below 10^n</returns>
+        /// <returns>The number of bouncy numbers below 10^n</returns>
         static BigInteger getBouncy(int n)
         {
             return BigInteger.Pow(10, n) - Functions.getnCk(9 + n, n) - (from i in Enumerable.Range(1, n) select Functions.getnCk(9 + i, i)).Aggregate((sum, next) => sum + next) + 10 * n;
