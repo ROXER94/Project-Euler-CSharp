9195a2b39d89377591eb922983c7a84566a34260
Robert Oxer <rdo26@cornell.edu>
2019-07-24 18:42:47 -0400

    Add files via upload

--- ProjectEuler/Problem174.cs
+++ ProjectEuler/Problem174.cs
@@ -12,18 +12,16 @@ namespace ProjectEuler
         static void P174()
         {
             int n = 1000000;
-            IDictionary<long, int> tilesDict = new Dictionary<long, int>();
+            IDictionary<int, int> tilesDict = new Dictionary<int, int>();
             for (int l = 3; l <= n / 4 + 1; l++)
                 for (int s = l - 2; s > 0; s -= 2)
                 {
                     int currentValue = l * l - s * s;
                     if (currentValue <= n)
-                    {
                         if (tilesDict.ContainsKey(currentValue))
                             tilesDict[currentValue]++;
                         else
                             tilesDict.Add(currentValue, 1);
-                    }
                     else break;
                 }
             Console.WriteLine((from i in tilesDict.Values where i <= 10 select 1).Sum());
