38c5cdefc7dad7b9433fe2f80bd82307daf8e8b4
Robert Oxer <rdo26@cornell.edu>
2019-07-26 21:45:13 -0400

    Add files via upload

--- ProjectEuler/Problem348.cs
+++ ProjectEuler/Problem348.cs
@@ -12,18 +12,16 @@ namespace ProjectEuler
         /// </summary>
         static void P348()
         {
-            IDictionary<long, int> palindromesDict = new Dictionary<long, int>();
+            IDictionary<int, int> palindromesDict = new Dictionary<int, int>();
             for (int i = 2; i < 30000; i++)
                 for (int j = 2; j < 1000; j++)
                 {
-                    long currentValue = i * i + j * j * j;
+                    int currentValue = i * i + j * j * j;
                     if (Functions.isPalindrome(currentValue.ToString()))
-                    {
                         if (palindromesDict.ContainsKey(currentValue))
                             palindromesDict[currentValue]++;
                         else
                             palindromesDict.Add(currentValue, 1);
-                    }
                 }
             Console.WriteLine((from i in palindromesDict.OrderByDescending(x => x.Value == 4).Take(5) select i.Key).Sum());
         }
