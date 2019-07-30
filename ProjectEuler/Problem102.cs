5659a4231e8fd564349700a46cea1466d4fbdc45
Robert Oxer <rdo26@cornell.edu>
2019-07-30 18:02:03 -0400

    Add files via upload

--- ProjectEuler/Problem102.cs
+++ ProjectEuler/Problem102.cs
@@ -29,10 +29,11 @@ namespace ProjectEuler
         /// <param name="y2">Int</param>
         /// <param name="x3">Int</param>
         /// <param name="y3">Int</param>
+        /// <param name="area">Double</param>
         /// <returns>The first barycentric coordinate of a triangle located at points (x1,y1), (x2,y2), and (x3,y3)</returns>
-        static double getBarycentricCoordinate1(int x1, int y1, int x2, int y2, int x3, int y3)
+        static double getBarycentricCoordinate1(int x1, int y1, int x2, int y2, int x3, int y3, double area)
         {
-            return 1 / (2 * getTriangleArea(x1, y1, x2, y2, x3, y3)) * (y1 * x3 - x1 * y3);
+            return 1 / (2 * area) * (y1 * x3 - x1 * y3);
         }
 
         /// <summary>
@@ -44,10 +45,11 @@ namespace ProjectEuler
         /// <param name="y2">Int</param>
         /// <param name="x3">Int</param>
         /// <param name="y3">Int</param>
+        /// <param name="area">Double</param>
         /// <returns>The second barycentric coordinate of a triangle located at points (x1,y1), (x2,y2), and (x3,y3)</returns>
-        static double getBarycentricCoordinate2(int x1, int y1, int x2, int y2, int x3, int y3)
+        static double getBarycentricCoordinate2(int x1, int y1, int x2, int y2, int x3, int y3, double area)
         {
-            return 1 / (2 * getTriangleArea(x1, y1, x2, y2, x3, y3)) * (x1 * y2 - y1 * x2);
+            return 1 / (2 * area) * (x1 * y2 - y1 * x2);
         }
 
         /// <summary>
@@ -58,14 +60,16 @@ namespace ProjectEuler
             int ans = 0;
             foreach (string line in File.ReadAllText(@"...\...\Resources\p102_triangles.txt").Split('\n'))
             {
-                int x1 = Int32.Parse(line.Split(',')[0]);
-                int y1 = Int32.Parse(line.Split(',')[1]);
-                int x2 = Int32.Parse(line.Split(',')[2]);
-                int y2 = Int32.Parse(line.Split(',')[3]);
-                int x3 = Int32.Parse(line.Split(',')[4]);
-                int y3 = Int32.Parse(line.Split(',')[5]);
-                double bCoordinate1 = getBarycentricCoordinate1(x1, y1, x2, y2, x3, y3);
-                double bCoordinate2 = getBarycentricCoordinate2(x1, y1, x2, y2, x3, y3);
+                string[] l = line.Split(',');
+                int x1 = Int32.Parse(l[0]);
+                int y1 = Int32.Parse(l[1]);
+                int x2 = Int32.Parse(l[2]);
+                int y2 = Int32.Parse(l[3]);
+                int x3 = Int32.Parse(l[4]);
+                int y3 = Int32.Parse(l[5]);
+                double area = getTriangleArea(x1, y1, x2, y2, x3, y3);
+                double bCoordinate1 = getBarycentricCoordinate1(x1, y1, x2, y2, x3, y3, area);
+                double bCoordinate2 = getBarycentricCoordinate2(x1, y1, x2, y2, x3, y3, area);
                 if (bCoordinate1 > 0 && bCoordinate2 > 0 && 1 - bCoordinate1 - bCoordinate2 > 0) ans++;
             }
             Console.WriteLine(ans);
