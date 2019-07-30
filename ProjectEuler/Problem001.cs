f229b27b344512725f760a75039e0068439df878
Robert Oxer <rdo26@cornell.edu>
2019-07-30 18:13:30 -0400

    Add files via upload

--- ProjectEuler/Problem001.cs
+++ ProjectEuler/Problem001.cs
@@ -1,16 +1,27 @@
 ﻿using System;
-using System.Linq;
 
 namespace ProjectEuler
 {
     partial class ProjectEuler
     {
         /// <summary>
+        /// Gets the sum of integers divisible by a number up to and including a limit
+        /// </summary>
+        /// <param name="n">Int</param>
+        /// <returns>The sum of integers divisible by n up to and including a limit</returns>
+        static int getSumDivisibleBy(int n, int limit)
+        {
+            int p = limit / n;
+            return n * p * (p + 1) / 2;
+        }
+
+        /// <summary>
         /// Calculates the sum of all the multiples of 3 or 5 below 1000
         /// </summary>
         static void P001()
         {
-            Console.WriteLine((from i in Enumerable.Range(0, 1000) where i % 3 == 0 || i % 5 == 0 select i).Sum());
+            int limit = 999;
+            Console.WriteLine(getSumDivisibleBy(3, limit) + getSumDivisibleBy(5, limit) - getSumDivisibleBy(15, limit));
         }
     }
 }
