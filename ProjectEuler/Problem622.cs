98a0270ac1ad5eff3096271e89b459ffe65f0bc5
Robert Oxer <rdo26@cornell.edu>
2019-08-09 20:57:55 -0400

    Add files via upload

--- ProjectEuler/Problem622.cs
+++ ProjectEuler/Problem622.cs
@@ -16,7 +16,7 @@ namespace ProjectEuler
             SortedSet<long> factors1 = Functions.getFactors((long)Math.Pow(2, e / 2) + 1);
             SortedSet<long> factors2 = Functions.getFactors((long)Math.Pow(2, e / 2) - 1);
             var Factors = (from i in factors1 from j in factors2 select i * j);
-            Console.WriteLine((from n in Factors.Skip(1) where Functions.getMultiplicativeOrder(2, n) == 60 select n + 1).Sum());
+            Console.WriteLine((from n in Factors.Skip(1) where Functions.getMultiplicativeOrder(2, n) == e select n + 1).Sum());
         }
     }
 }
